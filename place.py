fae648f6bd7748c68a6226422b374e4052ed07ab
aditibhatt32 <67057405+aditibhatt32@users.noreply.github.com>
2021-08-03 21:44:49 +0530

    place holders

---
+++ place.py
@@ -0,0 +1,14 @@
+title='\npython is easy\n'
+for char in title:
+     print(char,end='')
+for char in title:
+    if char=='y':
+     print('*',end='')
+    continue
+    print(char,end='')
+    for char in title:
+     if char=='y':
+      print('*',end='')
+      pass
+     print(char,end='')
+
